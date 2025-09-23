380c416b5282c3a8c2f4c9531be8acbd03f03f10
Utkarsh Pandey <utkarshji14@gmail.com>
2025-09-23 13:54:53 +0530

    Login, Register pages added

--- app/src/main/java/com/srmu/authentication/LoginActivity.java
+++ app/src/main/java/com/srmu/authentication/LoginActivity.java
@@ -1,6 +1,15 @@
 package com.srmu.authentication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +17,64 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
 
+public class LoginActivity extends AppCompatActivity {
+    //declare the java fields based on ui widgets
+    Button btn_login;
+    EditText et_username, et_password;
+    TextView tv_register;
+    SharedPreferences sp;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //to get the sharedPreferences file srmu from application
+        sp = getSharedPreferences("srmu", 0);
+        //initialization connect ui widget to java fields
+        btn_login = findViewById(R.id.btn_login);
+        et_password = findViewById(R.id.et_password);
+        et_username = findViewById(R.id.et_username);
+        tv_register = findViewById(R.id.tv_register);
+
+
+        //to click on the login button
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add validation to check the username and password are correct
+                if (et_username.getText().toString().equals(sp.getString("username", null))
+                        && et_password.getText().toString().equals(sp.getString("password", null)))
+                {
+                    //credentials are correct
+                    //redirect to MainActivity
+                    Intent home = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(home);
+                    finish();
+
+
+                }else {
+                    Toast.makeText(LoginActivity.this, "Credential Incorrect",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+
+        //to click on the don't have an account textview
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //redirect to MainActivity
+                Intent register = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(register);
+                finish();
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/srmu/authentication/RegisterActivity.java
+++ app/src/main/java/com/srmu/authentication/RegisterActivity.java
@@ -1,6 +1,12 @@
 package com.srmu.authentication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,68 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class RegisterActivity extends AppCompatActivity {
+    //declare the java fields based on ui widgets
+    EditText et_username, et_fullName, et_emailId, et_password, et_cPassword;
+    Button btn_register;
+    TextView tv_login;
+
+
+    SharedPreferences sp;
+    SharedPreferences.Editor ed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //to initialize the sharedPreferences
+        sp = getSharedPreferences("srmu", 0);
+        //to edit the sharedpreferences file
+        ed = sp.edit();
+
+
+        //intialization the java fields from ui widgets
+        et_fullName = findViewById(R.id.et_fullname);
+        et_cPassword = findViewById(R.id.et_cpassword);
+        et_password = findViewById(R.id.et_password);
+        et_username = findViewById(R.id.et_username);
+        et_emailId = findViewById(R.id.et_emailid);
+        btn_register = findViewById(R.id.btn_register);
+        tv_login = findViewById(R.id.tv_signin);
+
+
+        //to click on the register button
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //to save the user data into sharedpreferences file srmu in key value pair
+                ed.putString("username", et_username.getText().toString());
+                ed.putString("fullName", et_fullName.getText().toString());
+                ed.putString("emailId", et_emailId.getText().toString());
+                ed.putString("password", et_password.getText().toString());
+                ed.commit();
+                //redirect to MainActivity
+                Intent home = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
         });
+
+
+        //redirect to LoginActivity
+        tv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //redirect to MainActivity
+                Intent login = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
+
+
     }
 }
